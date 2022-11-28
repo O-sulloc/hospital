363c3d4ddf6521a612a7f4dc05e4386bf82891f6
O-sulloc <bujjaf@gmail.com>
2022-11-28 11:49:53 +0900

    init

---
+++ src/test/java/com/example/nationwidehospital/NationwideHospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nationwidehospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NationwideHospitalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
